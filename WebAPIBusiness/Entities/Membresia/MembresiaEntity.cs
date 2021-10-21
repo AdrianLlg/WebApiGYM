4bff15752ac585596b635ffdadaad183d9319a04
Adrian Ismael Llangari <adrianismael.llangari@udla.edu.ec>
2021-10-21 16:35:39 -0500

    Actualizacion del API MembresiasUsuario

--- WebAPIBusiness/Entities/Membresia/MembresiaEntity.cs
+++ WebAPIBusiness/Entities/Membresia/MembresiaEntity.cs
@@ -8,13 +8,12 @@ namespace WebAPIBusiness.Entities.Membresia
 {
     public class MembresiaEntity
     {
-        public int disciplinaID { get; set; }
+        public string nombreMembresia { get; set; }
+        public int precioMembresia { get; set; }
         public string nombreDisciplina { get; set; }
-        public decimal precio { get; set; }
-        public int numClasesDisponibles { get; set; }
         public DateTime fechaPago { get; set; }
         public DateTime fechaLimite { get; set; }
-        public string nombreMembresia { get; set; }
+        public int numClasesDisponibles { get; set; }
 
     }
 }
